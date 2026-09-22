fe508c80bda15bd7585310440086bd811e3c7428
Kabir99-login <kabirpratapsingh99@gmail.com>
2026-09-22 16:53:37 +0530

    Sample Project

---
+++ CRMSampleProject/src/test/java/com/vtiger/contact/ContactTest.java
@@ -0,0 +1,31 @@
+package com.vtiger.contact;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContact() {
+
+		String URl = System.getProperty("url", "http://testenv");
+		String Browser = System.getProperty("browser", "chrome");
+		String username = System.getProperty("username", "admin");
+		String password = System.getProperty("password", "admin");
+
+		System.out.println(URl);
+		System.out.println(Browser);
+		System.out.println(username);
+		System.out.println(password);
+
+	}
+	@Test
+	public void createContactWithDropdown() {
+
+		
+		System.out.println("Step1");
+		System.out.println("Step2");
+		System.out.println("Step3");
+		System.out.println("Step4");
+
+	}
+
+}
---
+++ CRMSampleProject/src/test/java/com/vtiger/organization/OrganizationTest.java
@@ -0,0 +1,30 @@
+package com.vtiger.organization;
+
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	
+	@Test
+	public void createOrganization() {
+
+		
+		System.out.println("Step1");
+		System.out.println("Step2");
+		System.out.println("Step3");
+		System.out.println("Step4");
+
+	}
+	
+	
+	@Test
+	public void createOrgWithDropdown() {
+
+		
+		System.out.println("Step1");
+		System.out.println("Step2");
+		System.out.println("Step3");
+		System.out.println("Step4");
+
+	}
+
+}
